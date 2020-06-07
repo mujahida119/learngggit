ebd93e9ad4033b10853573b2368832c31b0bb81d
mujahida <mujahidaafridi0307@gmail.com>
2020-06-07 19:49:51 +0500

    Now to ready commit

---
+++ app.js
@@ -0,0 +1,149 @@
+chapter:01
+
+//alert("");//
+
+//alert('JavaScript Alert\nError! pleace enter a valid password');//
+
+//alert('JavaScript Alert\nWelcome to JS Land...\nHappy Coding!');//
+
+//alert('welcome to JS Land..')//
+
+//alert('Happy Coding!\nParent this page from creating addtional dialog.')//
+
+//alert('Hello...I can run JS through my web browsers console');//
+
+chapter:02
+
+//alert('Jhone Doe');//
+
+//alert('15 years old');//
+
+//alert('Certified Mobile Application Development');//
+
+//alert('My email address is example@example .com');//
+
+//alert('I am tryingto learn from the book A smarter\nway to learn Javascript');//
+
+//document.writeln('Yah! I can write HTML content through JavaScript');//
+
+//alert('--------------&&&------------');//
+
+//alert('PIZZA\nPIZZ\nPIZ\nPI\nP');//
+
+chapter:03
+
+//alert('I am 15 years old');//
+
+//alert('You have visited this site 14 times');//
+
+//document.writeln('My birth year is 1999 <br>Date type of my decleard is number');//
+
+//document.writeln('John Doe ordered 5 T-shirt(s)on XYZClothig store');//
+
+chaper:04
+ 
+//document.writeln('Variable names can only contain ,numbers, $ and_.For example:$my_1stVariable <br> variable must begin with letter, $ or_.For example : $name,_name or name <br> Variable name are case sensitive <br> Variable name should not be JS keyword')
+
+chapter:05
+
+//document.writeln('Sum of  3  and  5 is 8');//
+
+//var x="";//
+
+//var x= 5;
+
+//var f = 2;
+ //var g = 3; 
+//var h =f + g;  
+//alert("Initail value: " +  h )
+
+//var x = a ++
+
+ //var f = 2;
+ //var g = ++ f ;                
+//var h = f+g;
+//alert("Value after increament: " + h );
+
+
+
+//var a= 9;
+//var b=4;
+//var c=b+a;
+//alert("Value after Addition : " + c);
+
+//var x=3;
+//var y=3;
+//var z= x%y;
+//alert("the reminder is : " + z);
+
+//var x=600;
+//var y=5;
+//var z= x*y;
+//document.writeln("Total cost to buy 5 tickets to a movieis : " + z );
+
+
+//for(var i =1; i<=10; i++){
+  //  document.write("4" + "x" + i + "=" + 2*i + "<br>");}
+
+
+
+
+
+
+
+
+
+
+
+  chapter:13-15
+
+//var a =[" ", " ", " "];
+ //alert('');
+
+ //var a =["ali","khan","zayan"];
+ //alert(a);
+
+ //var a =[1,2,3,4];
+ //alert(a);
+
+ //var a =[138,"ali",12,"ghouse",123];
+ //alelrt(a[0]);
+ //alert(a[1]);
+ //alert(a[2]);
+ //alert(a[3]);
+ //alert(a[4]);
+
+ //var x =[1, 2, 3,];
+
+ //var a =[true,false];
+
+ //var names = ["SSA" , "HSA","BCA","BS","BCOM","MS","M.Phil","PhD"];
+ //for(var i=0; i < names.length; i++)
+ //{
+  // console.log(names[i])
+ //}
+
+
+ //var arr = ["this" ,"is", "my" ,"cat"];
+   //console.log (arr)
+
+   //var arr = ["this" "is"];
+  // Console.log(arr)
+
+  //var arr = ["keyboard","mouse","printer","out","monitor"];
+    //console.log(arr)
+
+
+    //var names = ["out:","keyboard","out:", "mouse","out:","printer","out:","monitor"];
+     //for(var i= 0; i < names.length; i++ ){
+      // console.log( names[i])
+     //}
+
+     
+ 
+
+
+
+
+
+
